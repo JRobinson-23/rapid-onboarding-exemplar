d1d7f18417a200ed82f58cecd9e1791b385cedcd
James Robinson <james.robinson@servian.com>
2022-03-17 23:52:57 +0000

    test

--- models/staging/tpch/stg_tpch_customers.sql
+++ models/staging/tpch/stg_tpch_customers.sql
@@ -10,6 +10,7 @@ renamed as (
         -- ids
         c_custkey as customer_id,
         c_nationkey as nation_id,
+        row_number() as c_extra_id, 
 
         -- descriptions
         c_name as name,
